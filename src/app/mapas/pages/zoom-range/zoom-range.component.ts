fcad06e5bef6b0e300a3c6d0a65f20803e78a9d6
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2022-01-11 13:01:47 -0300

    mov map & onDestroy

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -28,13 +28,20 @@ import * as mapboxgl from 'mapbox-gl';
  * por ciclo de vida en el oninit no se pued einicializar debido que el mapa carga despues de iniciar la aplicaciony  no con la aplicación.
  * es por esto que se necesita el afterInit para que logre inizializar el mapa
  */
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef
   mapa!: mapboxgl.Map;
 
-  zoomLevel : number  =10;
+  zoomLevel : number  = 10;
+  center:[number, number] = [-70.66060651142959,-33.44555948505073]
+
   constructor() { }
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', ()=>{});
+    this.mapa.off('zoomend', ()=>{});
+    this.mapa.off('move', ()=>{});
+  }
 
       
     ngAfterViewInit(): void {
@@ -44,18 +51,28 @@ export class ZoomRangeComponent implements AfterViewInit {
       // mapa solventar el container debemos colocar el id de elemento que contendra el mapa.
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-70.66060651142959,-33.44555948505073],
+      center: this.center ,
       zoom: this.zoomLevel
     });
+    // on es como un event listener y es un metodo de mapbox
 
+    // obtener el zoom del mapa
     this.mapa.on('zoom', ()=>{
       this.zoomLevel = this.mapa.getZoom();
     });
-    // on es como un event listener y es un metodo de mapbox
+
+    // restringir el zoom a 18
     this.mapa.on('zoomend',()=>{
       if (this.mapa.getZoom()>18) {
         this.mapa.zoomTo(18);
       }
+    })
+
+    // Movimiento del mapa
+    this.mapa.on('move',(event)=>{
+      const target = event.target;
+      const {lng, lat} = target.getCenter();
+      this.center = [lng, lat];
     })
   }
 
